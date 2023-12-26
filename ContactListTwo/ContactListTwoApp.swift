d14112472438f883c02d9f32f35c5a02971b229c
tatanadubova <tatyana.dubova.1986@gmail.com>
2023-12-26 13:01:33 +0300

    Initial Commit

---
+++ ContactListTwo/ContactListTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListTwoApp.swift
+//  ContactListTwo
+//
+//  Created by Татьяна Дубова on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
